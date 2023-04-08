882f6685c600498cae62a216441700d4099aebae
am <am@acorello.it>
2023-04-08 18:03:35 +0200

    Solve coin-change

--- 01_04b/main.go
+++ 01_04b/main.go
@@ -2,45 +2,122 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 )
 
-// coin contains the name and value of a coin
-type coin struct {
-	name  string
-	value float64
+type coin uint16
+
+const (
+	c100p coin = 100
+	c50p  coin = 50
+	c20p  coin = 20
+	c10p  coin = 10
+	c05p  coin = 5
+	c02p  coin = 2
+	c01p  coin = 1
+)
+
+const coinCount = 7
+
+var coinSizesInDescendingOrd = [coinCount]coin{
+	c100p,
+	c50p,
+	c20p,
+	c10p,
+	c05p,
+	c02p,
+	c01p,
 }
 
-// coins is the list of values available for making change.
-var coins = []coin{
-	{name: "1 pound", value: 1},
-	{name: "50 pence", value: 0.50},
-	{name: "20 pence", value: 0.20},
-	{name: "10 pence", value: 0.10},
-	{name: "5 pence", value: 0.05},
-	{name: "1 penny", value: 0.01},
+var coinNames [coinCount]string
+
+func init() {
+	for i, c := range coinSizesInDescendingOrd {
+		switch c {
+		case 100:
+			coinNames[i] = "1 pound"
+		case 50, 20, 10, 5, 2:
+			coinNames[i] = fmt.Sprintf("%d pence", c)
+		case 1:
+			coinNames[i] = "1 penny"
+		default:
+			log.Panicf("missing case for %d", c)
+		}
+	}
+}
+
+func (I coin) Name() string {
+	for i, c := range coinSizesInDescendingOrd {
+		if c == I {
+			return coinNames[i]
+		}
+	}
+	log.Panicf("missing name for %d", I)
+	return ""
 }
 
 // calculateChange returns the coins required to calculate the
-func calculateChange(amount float64) map[coin]int {
-	panic("NOT IMPLEMENTED")
+func calculateChange(amount uint64) change {
+	change := NewChange()
+	var quantity uint64
+	for _, nextCoin := range coinSizesInDescendingOrd {
+		quantity, amount = nextCoin.DivMod(amount)
+		change.Add(nextCoin, quantity)
+		if amount == 0 {
+			break
+		}
+	}
+	return change
+}
+
+type change map[coin]uint64
+
+func NewChange() change {
+	return make(change)
+}
+
+func (I coin) DivMod(amount uint64) (q uint64, r uint64) {
+	i := uint64(I)
+	return amount / i, amount % i
+}
+
+func (I change) Add(c coin, q uint64) {
+	I[c] += q
 }
 
 // printCoins prints all the coins in the slice to the terminal.
-func printCoins(change map[coin]int) {
-	if len(change) == 0 {
-		log.Println("No change found.")
+func (I change) printCoins() {
+	if len(I) == 0 {
+		fmt.Println("No change found.")
 		return
 	}
-	log.Println("Change has been calculated.")
-	for coin, count := range change {
-		log.Printf("%d x %s \n", count, coin.name)
+	fmt.Println("Change has been calculated.")
+	for _, nextCoin := range coinSizesInDescendingOrd {
+		count := I[nextCoin]
+		if count > 0 {
+			fmt.Printf("%d x %s \n", count, nextCoin.Name())
+		}
 	}
 }
 
+var amountRegex = regexp.MustCompile(`^\s*(\d+)(?:\.(\d{1,2}))?\s*$`)
+
 func main() {
-	amount := flag.Float64("amount", 0.0, "The amount you want to make change for")
+	amountFlag := flag.String("amount", "", "The amount you want to make change for")
 	flag.Parse()
-	change := calculateChange(*amount)
-	printCoins(change)
+	parts := amountRegex.FindStringSubmatch(*amountFlag)
+	if parts == nil {
+		log.Fatal("Invalid format:", *amountFlag)
+	}
+	amountUnit, _ := strconv.ParseUint(parts[1], 10, 64)
+	var amountDec uint64
+	if len(parts) > 2 {
+		amountDec, _ = strconv.ParseUint(parts[2], 10, 64)
+	}
+	amount := amountUnit*100 + amountDec
+	change := calculateChange(amount)
+	change.printCoins()
 }
