d089ea3d268740f4ed4f5df3bafc6aee645c8ed6
am <am@acorello.it>
2023-04-11 11:55:00 +0200

    Solve and refactor 01_05b

--- 01_05b/main.go
+++ 01_05b/main.go
@@ -5,22 +5,68 @@ import (
 	"flag"
 	"log"
 	"os"
+	"sort"
 )
 
 const path = "items.json"
 
 // SaleItem represents the item part of the big sale.
 type SaleItem struct {
-	Name           string  `json:"name"`
-	OriginalPrice  float64 `json:"originalPrice"`
-	ReducedPrice   float64 `json:"reducedPrice"`
+	Name          string  `json:"name"`
+	OriginalPrice float64 `json:"originalPrice"`
+	ReducedPrice  float64 `json:"reducedPrice"`
+}
+
+type Item struct {
+	SaleItem
 	SalePercentage float64
 }
 
-// matchSales adds the sales procentage of the item
-// and sorts the array accordingly.
-func matchSales(budget float64, items []SaleItem) []SaleItem {
-	panic("NOT IMPLEMENTED")
+// Here is a list:
+//
+//  1. Item 1.
+//
+//     Subitem 1.
+//
+//     Subitem 2.
+//
+//  2. Item 2.
+//
+//  3. Item 3.
+func NewItem(i SaleItem) Item {
+	pcd := ((i.OriginalPrice / i.ReducedPrice) - 1) * 100
+	return Item{i, pcd}
+}
+
+// matchSales adds the sales procentage of the item and sorts the array accordingly.
+
+// I'm assuming the match can have at lest the two following objectives
+//
+// N. OBJECTIVE ->
+//
+//	   SORTING-STRATEGY
+//
+//	1. maximize the number of items ->
+//	   by ReducedPrice ascending
+//
+//	2. present the most discounted first ->
+//	   by SalePercentage descending
+//
+// either way, I'm assuming I should not mutate the input slice
+func matchSales(budget float64, items []Item) []Item {
+	if budget <= 0 {
+		return nil
+	}
+	var res []Item
+	for _, item := range items {
+		if item.ReducedPrice <= budget {
+			res = append(res, item)
+		}
+	}
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].SalePercentage > res[j].SalePercentage
+	})
+	return res
 }
 
 func main() {
@@ -33,20 +79,20 @@ func main() {
 }
 
 // printItems prints the items and their sales.
-func printItems(items []SaleItem) {
+func printItems(items []Item) {
 	log.Println("The BIG sale has started with our amazing offers!")
 	if len(items) == 0 {
-		log.Println("No items found.:( Try increasing your budget.")
+		log.Println("No items found. Try increasing your budget.")
 	}
 	for i, r := range items {
-		log.Printf("[%d]:%s is %.2f OFF! Get it now for JUST %.2f!\n", 
-		i, r.Name, r.SalePercentage, r.ReducedPrice)
+		log.Printf("[%d]:%s is %.0f%% OFF! Get it now for JUST %.2f!\n",
+			i, r.Name, r.SalePercentage, r.ReducedPrice)
 	}
 }
 
-// importData reads the raffle entries from file and 
+// importData reads the raffle entries from file and
 // creates the entries slice.
-func importData() []SaleItem {
+func importData() (res []Item) {
 	file, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
@@ -57,6 +103,9 @@ func importData() []SaleItem {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	return data
+	res = make([]Item, len(data))
+	for i := range data {
+		res[i] = NewItem(data[i])
+	}
+	return
 }
